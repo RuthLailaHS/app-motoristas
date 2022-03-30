c441b225150e812da989ab219ecdff23fb49cd5e
Ruth Laila Silva <94913764+RuthLailaHS@users.noreply.github.com>
2022-03-30 11:30:11 -0300

    Atualizações Banco de Dados

---
+++ database/migrations/2022_03_30_133757_create_veiculos_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('veiculos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_user');
+            $table->foreign('id_user')->references('id')->on('users');
+            $table->string('marca');
+            $table->string('modelo');
+            $table->integer('ano');
+            $table->string('cor');
+            $table->float('capacidade_litros', 8, 2);
+            $table->string('tipo_combustivel');
+            $table->integer('pneus');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('veiculos');
+    }
+};
---
+++ database/migrations/2022_03_30_133921_create_perguntas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('perguntas', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_user');
+            $table->foreign('id_user')->references('id')->on('users');
+            $table->text('pergunta');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('perguntas');
+    }
+};
